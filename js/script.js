9467fc572902659ffb8291bec79d614c77f55dc1
parix1999 <parisottoedoardo@gmail.com>
2021-06-07 16:56:21 +0200

    burger-js-4.0

--- js/script.js
+++ js/script.js
@@ -3,9 +3,10 @@ var onClick = document.getElementById('button');
 var outPutBoxes = document.getElementById('boxes-content');
 
 var costoHamburger = 50;
+var conto = 50;
 outPutBoxes.innerHTML = '€ ' + 50; 
-var conto = 0;
 
+// Implementazione, codice che calcola le aggiunte Burger:
 
 // Comando JS per il click e l'inizio del calcolo e iterazione
 onClick.addEventListener('click', function(){         
@@ -13,13 +14,11 @@ onClick.addEventListener('click', function(){
     var checkboxes = document.getElementsByClassName('box');
     for (var x = 0; x < checkboxes.length; x++) {
         // Verifica se c'è stato il check:
-        if (parseInt(checkboxes[x]).checked === true ) {
+        if (checkboxes[x].checked === true ) {
             console.log(checkboxes[x].value);
+            var som = conto += parseInt(checkboxes[x].value);
         }
-    var som = conto += parseInt(checkboxes[x]);
-
     }
-
     outPutBoxes.innerHTML = '€' + som;
 
 
