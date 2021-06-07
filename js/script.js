ecefa22fb121702e3f673f7c476a106a94b573d0
parix1999 <parisottoedoardo@gmail.com>
2021-06-07 15:22:06 +0200

    burger-js-1.0

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Creo la mia variabile dove si scatenerà l'input per la somma degli elementi
+var onClick = document.getElementById('button');
+
+// Comando JS per il click e l'inizio del calcolo e iterazione
+onClick.addEventListener('click', function(){         
+    // Comandi per iterare e tenere in conto l'input dell'utente:
+    
+
+
+
+});
