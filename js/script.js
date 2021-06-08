a932810d561bdd8d652a3562b61dced19fd900a7
parix1999 <parisottoedoardo@gmail.com>
2021-06-08 12:48:39 +0200

    burger-js-1.0.0

--- js/script.js
+++ js/script.js
@@ -9,50 +9,43 @@ var onClick = document.getElementById('button');
 var outPutBoxes = document.getElementById('boxes-content');
 
 outPutBoxes.innerHTML = '€ ' + 50; 
-var discaunt20 = ['june20','sconto20','burger20'];
-var discaunt10 = ['june10','sconto10','burger10'];
-
+var lista20 = ['june20','sconto20','burger20'];
+var lista10 = ['june10','sconto10','burger10'];
+var price = 50;
 
 // Implementazione, codice che calcola le aggiunte Burger:
 // Comando JS per il click e l'inizio del calcolo e iterazione
 onClick.addEventListener('click', function(){         
     // Comandi per iterare e tenere in conto l'input dell'utente:
     var checkboxes = document.getElementsByClassName('box');
-    var costoHamburger = 50;
+    var costoHamburger = price;
 
     for (var x = 0; x < checkboxes.length; x++) {
         // Verifica se c'è stato il check:
         if (checkboxes[x].checked === true ) {
-            var somma = costoHamburger += parseInt(checkboxes[x].value);
-            outPutBoxes.innerHTML = '€' + somma.toFixed(2);
-        
+            costoHamburger += parseInt(checkboxes[x].value);
         }       
     }
 
-    var couponD = document.getElementById('coupon');
-    for (var i = 0; i < discaunt20.length; i++) {
-        if (discaunt20[i] === couponD.value) {
-            var discount = 20;
-            var discauntCalc20 = (somma * discount) / 100;
-            var calc20 = somma - discauntCalc20;
-            outPutBoxes.innerHTML = '€' + calc20.toFixed(2);
-        }
-
-    } 
-    
-    for (var z = 0; z < discaunt10.length; z++ ) {
-        if (discaunt10[z] === couponD.value) {
-            var discount = 10; 
-            var discauntCalc10 = (somma * discount) / 100;
-            var calc10 = somma - discauntCalc10;
-            outPutBoxes.innerHTML = '€' + calc10.toFixed(2);
+    var coupon = document.getElementById('coupon');
+    for (var i = 0; i < lista20.length; i++) {
+        if (lista20[i] === coupon.value) {
+            var sconto = 20;
+           
         }
 
     }
-    if (couponD.value.lenght > 0 && discaunt20[i]) {
-        alert('no');
-    } 
+    for (var z = 0; z < lista10.length; z++) {
+        if (lista10[z === coupon.value]) {
+            var sconto = 10;
+        }
 
+    }
+
+    costoHamburger = costoHamburger * ((100 - sconto) / 100);
+    outPutBoxes.innerHTML = '€' + costoHamburger.toFixed(2);
+
+    
 });
 
 
