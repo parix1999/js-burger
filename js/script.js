4573e3421bb973eeb598d607c221bef84b945d6c
parix1999 <parisottoedoardo@gmail.com>
2021-06-08 12:03:51 +0200

    burger-js-9.0

--- js/script.js
+++ js/script.js
@@ -8,9 +8,10 @@ document.getElementById('name').innerHTML =' Nome panino:  ' + nome;
 var onClick = document.getElementById('button');
 var outPutBoxes = document.getElementById('boxes-content');
 
-outPutBoxes.innerHTML = '€ ' + 50.00; 
-var discount = 20;
-var discuountCode = 'ciao'
+outPutBoxes.innerHTML = '€ ' + 50; 
+var discaunt20 = ['june20','sconto20','burger20'];
+var discaunt10 = ['june10','sconto10','burger10'];
+
 
 // Implementazione, codice che calcola le aggiunte Burger:
 // Comando JS per il click e l'inizio del calcolo e iterazione
@@ -25,16 +26,32 @@ onClick.addEventListener('click', function(){
             var somma = costoHamburger += parseInt(checkboxes[x].value);
             outPutBoxes.innerHTML = '€' + somma.toFixed(2);
         
+        }       
+    }
+
+    var couponD = document.getElementById('coupon');
+    for (var i = 0; i < discaunt20.length; i++) {
+        if (discaunt20[i] === couponD.value) {
+            var discount = 20;
+            var discauntCalc20 = (somma * discount) / 100;
+            var calc20 = somma - discauntCalc20;
+            outPutBoxes.innerHTML = '€' + calc20.toFixed(2);
         }
-        var couponD = document.getElementById('coupon');         
-        
-    }
+
+    } 
     
-    if (couponD.value === discuountCode) {
-        var discauntCalc = (somma * discount) / 100; 
-        var calcSconto = somma - discauntCalc;
-        outPutBoxes.innerHTML = '€' + calcSconto.toFixed(2);
+    for (var z = 0; z < discaunt10.length; z++ ) {
+        if (discaunt10[z] === couponD.value) {
+            var discount = 10; 
+            var discauntCalc10 = (somma * discount) / 100;
+            var calc10 = somma - discauntCalc10;
+            outPutBoxes.innerHTML = '€' + calc10.toFixed(2);
+        }
+
     }
+    if (couponD.value.lenght > 0 && discaunt20[i]) {
+        alert('no');
+    } 
 
 });
 
